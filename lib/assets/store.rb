bbfad874100acdd9e1862dd00f38b35d63888329
Vidar <vidar@fugroup.net>
2017-01-01 05:29:59 +0100

    Deep assets

--- lib/assets/store.rb
+++ lib/assets/store.rb
@@ -1,42 +1,55 @@
-# Represents an asset
-
 module Asset
+  # # # # # # # # #
+  # The Store class is responsible for compressing your assets if possible,
+  # or serve the file directly.
+  #
+  # It displays all files in your manifest.yml
+  # if you are running production mode, if not it displays compressed version.
+  #
 
   class Store
     class << self; attr_accessor :manifest; end
     @manifest = YAML.load_file(File.join(APP_ASSETS, 'manifest.yml'))
 
-    attr_accessor :type, :name, :base, :md5, :key, :files
+    attr_accessor :type, :path, :name, :base, :md5, :key, :files
 
     def initialize(path, type = path.split('.')[-1])
       @type = type
-      @name = path.split('/')[-1]
-      @name =~ /(.*)-([a-f0-9]{1,32})\.(.*)/
-      @name = @name.gsub("-#{@md5}", '') if (@md5 = $2)
+
+      # Set up path
+      @path = path
+      @path =~ /(.*)-([a-f0-9]{1,32})\.(.*)/
+      @path = @path.gsub("-#{@md5}", '') if (@md5 = $2)
+
+      # Name is the name with extension
+      @name = @path.split('/')[-1]
+
+      # Base is the name without extension
       @base = $1 || @name.rpartition('.')[0]
+
       @key = "#{@base}#{@md5 ? "-#{@md5}" : ''}.#{@type}"
       @files = []
 
+      # If 'application', we load via manifest
       if @base == 'application'
-        self.class.manifest[@type].each do |p|
-          @files << abs(p)
-        end
+        self.class.manifest[@type].each{|p| @files << abs(p)}
       else
-        @files << abs(@name)
+        @files << abs(@path)
       end
 
       # Remove nil
+      # TODO: Move away to avoid disk access
       @files.delete_if{|f| !File.file?(f)}
     end
 
     # The cached content
     def cached
       begin
-        File.read(tmp(@key)) if @md5 and @files.any?
+        File.read(cache) if @md5 and @files.any?
       rescue
         # No cache found, cache it
         compressed.tap do |c|
-          File.open(tmp(@key), 'w'){|f| f.write(c)}
+          File.open(cache, 'w'){|f| f.write(c)}
           puts "Cached!: #{@key}"
         end
       end
@@ -60,7 +73,7 @@ module Asset
 
     # The content on disk
     def content
-      File.read(@files[0]) rescue nil
+      @content ||= File.read(@files[0]) rescue nil
     end
 
     # File absolute path, nil if it doesn't exist
@@ -69,19 +82,20 @@ module Asset
     end
 
     # Find the cache path
-    def tmp(f)
-      File.join('tmp', f)
+    def cache
+      File.join('tmp', @key)
     end
 
     # Print data
-    def print
+    def to_s
       puts "TYPE: #{@type}"
+      puts "PATH: #{@path}"
       puts "NAME: #{@name}"
       puts "BASE: #{@base}"
       puts "MD5: #{@md5}"
       puts "KEY: #{@key}"
       puts "FILES: #{@files.inspect}"
-      puts "CONTENT: #{@content}"
+      puts "CONTENT: #{content}"
     end
   end
 end
--- test/store_test.rb
+++ test/store_test.rb
@@ -1,12 +1,65 @@
 test 'Item'
 
-test 'app css no md5, non-existing'
+test 'app css deep md5, existing'
+
+key = 'lib/app.min-452791b9192e41f88cebdd4b9f08543e.css'
+
+store = ::Asset::Store.new(key)
+is store.type, 'css'
+is store.name, :a? => String
+is store.path, :a? => String
+is store.base, :a? => String
+is store.md5, String
+is store.key, String
+is store.files, :a? => Array
+is store.files.size, :gt => 0
+is store.content, String
+is store.cached, String
+is store.cached.size, :gt => 0
+
+
+test 'app deep path css no md5, non-existing'
+
+key = 'lib/appylappy.css'
+
+store = ::Asset::Store.new(key)
+is store.type, 'css'
+is store.name, :a? => String
+is store.path, :a? => String
+is store.base, :a? => String
+is store.md5, nil
+is store.key, String
+is store.files, :a? => Array
+is store.files.size, 0
+is store.content, nil
+is store.cached, nil
+
+
+test 'app deep path css no md5, existing'
+
+key = 'lib/app.min.css'
+
+store = ::Asset::Store.new(key)
+is store.type, 'css'
+is store.name, :a? => String
+is store.path, :a? => String
+is store.base, :a? => String
+is store.md5, nil
+is store.key, String
+is store.files, :a? => Array
+is store.files.size, :gt => 0
+is store.content, String
+is store.cached, nil
+
+
+test 'app css md5, existing'
 
 key = 'appylappy.css'
 
 store = ::Asset::Store.new(key)
 is store.type, 'css'
 is store.name, :a? => String
+is store.path, :a? => String
 is store.base, :a? => String
 is store.md5, nil
 is store.key, String
@@ -23,6 +76,7 @@ key = 'appylappy-9e13f5f27b151f2c03d2966e06adc7b7.css'
 store = ::Asset::Store.new(key)
 is store.type, 'css'
 is store.name, :a? => String
+is store.path, :a? => String
 is store.base, :a? => String
 is store.md5, String
 is store.key, String
@@ -39,6 +93,7 @@ key = 'appylappy.js'
 store = ::Asset::Store.new(key)
 is store.type, 'js'
 is store.name, :a? => String
+is store.path, :a? => String
 is store.base, :a? => String
 is store.md5, nil
 is store.key, String
@@ -55,6 +110,7 @@ key = 'appylappy-9e13f5f27b151f2c03d2966e06adc7b7.js'
 store = ::Asset::Store.new(key)
 is store.type, 'js'
 is store.name, :a? => String
+is store.path, :a? => String
 is store.base, :a? => String
 is store.md5, String
 is store.key, String
@@ -71,6 +127,7 @@ key = 'app-9e13f5f27b151f2c03d2966e06adc7b7.js'
 store = ::Asset::Store.new(key)
 is store.type, 'js'
 is store.name, :a? => String
+is store.path, :a? => String
 is store.base, :a? => String
 is store.md5, String
 is store.key, String
@@ -80,6 +137,7 @@ is store.content, String
 is store.cached, String
 is store.cached.size, :gt => 0
 
+
 test 'application css, double dot name, md5, non-existing'
 
 key = 'app.min-452791b9192e41f88cebdd4b9f08543e.css'
@@ -87,6 +145,7 @@ key = 'app.min-452791b9192e41f88cebdd4b9f08543e.css'
 store = ::Asset::Store.new(key)
 is store.type, 'css'
 is store.name, :a? => String
+is store.path, :a? => String
 is store.base, :a? => String
 is store.md5, String
 is store.key, String
@@ -97,11 +156,13 @@ is store.cached, nil
 
 
 test 'application css, md5'
+
 key = 'application-9e13f5f27b151f2c03d2966e06adc7b7.css'
 
 store = ::Asset::Store.new(key)
 is store.type, 'css'
 is store.name, :a? => String
+is store.path, :a? => String
 is store.base, :a? => String
 is store.md5, String
 is store.key, String
@@ -119,6 +180,7 @@ key = 'application-9e13f5f27b151f2c03d2966e06adc7b7.js'
 store = ::Asset::Store.new(key)
 is store.type, 'js'
 is store.name, :a? => String
+is store.path, :a? => String
 is store.base, :a? => String
 is store.md5, String
 is store.key, String
@@ -128,12 +190,15 @@ is store.content, String
 is store.cached, String
 is store.cached.size, :gt => 0
 
+
 test 'existing file, no md5'
 
 key = 'app.js'
+
 store = ::Asset::Store.new(key)
 is store.type, 'js'
 is store.name, :a? => String
+is store.path, :a? => String
 is store.base, :a? => String
 is store.md5, nil
 is store.key, String
